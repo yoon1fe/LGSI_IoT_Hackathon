d3481c466ba53bedd28743bca7aab51cbb9f4443
yoon1fe <34064616+yoon1fe@users.noreply.github.com>
2019-11-12 20:08:14 +0900

    Add files via upload

---
+++ face_identification/retrain_run_inference.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""Inception v3 architecture 모델을 retraining한 모델을 이용해서 이미지에 대한 추론(inference)을 진행하는 예제"""
+
+import numpy as np
+import tensorflow as tf
+
+imagePath = '/tmp/registered.jpg'                                         # 추론을 진행할 이미지 경로
+modelFullPath = '/tmp/output_graph.pb'                                      # 읽어들일 graph 파일 경로
+labelsFullPath = '/tmp/output_labels.txt'                                   # 읽어들일 labels 파일 경로
+
+def create_graph():
+    """저장된(saved) GraphDef 파일로부터 graph를 생성하고 saver를 반환한다."""
+    # 저장된(saved) graph_def.pb로부터 graph를 생성한다.
+    with tf.gfile.FastGFile(modelFullPath, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+
+def run_inference_on_image():
+    answer = None
+    flag = 0
+    if not tf.gfile.Exists(imagePath):
+        tf.logging.fatal('File does not exist %s', imagePath)
+        return answer
+
+    image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
+
+    # 저장된(saved) GraphDef 파일로부터 graph를 생성한다.
+    create_graph()
+
+    with tf.Session() as sess:
+
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        predictions = sess.run(softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+
+        top_k = predictions.argsort()[-5:][::-1]  # 가장 높은 확률을 가진 5개(top 5)의 예측값(predictions)을 얻는다.
+        f = open(labelsFullPath, 'rb')
+        lines = f.readlines()
+        labels = [str(w).replace("\n", "") for w in lines]
+        for node_id in top_k:
+            human_string = labels[node_id]
+            human_name = human_string[2:-3]
+            score = predictions[node_id]
+            if score >= 0.85:   #본인 확인 percentage
+                flag = 1
+                print('Hello %s, the gate is opened (score = %.1f)' %(human_name, score*100))
+
+        if flag == 0:
+            print('Unregistered User')
+        answer = labels[top_k[0]]
+        return answer
+
+
+if __name__ == '__main__':
+    run_inference_on_image()
